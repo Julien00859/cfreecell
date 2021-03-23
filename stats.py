4599280c6b56af69c0318fea973c464318e2360d
Julien Castiaux <julien.castiaux@gmail.com>
2021-03-23 02:59:59 +0100

    even better ctrl-c handling

--- stats.py
+++ stats.py
@@ -2,43 +2,44 @@
 
 import asyncio
 import itertools
+import functools
 import os
 import pathlib
 import subprocess as sp
 import signal
 import sys
 
-WORKERS = os.cpu_count() - 3
+WORKERS = os.cpu_count() - 1  # Let's be nice and leave one cpu free
 ROOT = pathlib.Path(__file__).resolve().absolute().parent
 counter = itertools.count(int(sys.argv[1]) if len(sys.argv) > 1 else 0)
+times_path = ROOT / 'stats' / 'times.txt'
 
 
 async def run(event):
+
     while not event.is_set():
         seed = next(counter)
         with (ROOT / 'stats' / f'game_{seed:06d}.txt').open('wb') as fd:
             proc = await asyncio.create_subprocess_exec(
-                '/usr/bin/time',
-                  '-a', '-o', str(ROOT / 'stats' / 'times.txt'),
-                  '-f', f'seed {seed:06d} code %x wall %e user %U sys %S mem %Mkb',
-                  str(ROOT / 'freecell'), str(seed),
+                '/usr/bin/timeout', '-v', '10',
+                '/usr/bin/time', '-a', '-o', str(times_path),
+                '-f', f'seed {seed:06d} code %x wall %e user %U sys %S mem %Mkb',
+                str(ROOT / 'freecell'), str(seed),
                 stdout=fd, stderr=fd)
-            try:
-                await asyncio.wait_for(proc.wait(), 10)
-            except asyncio.TimeoutError:
-                proc.terminate()
-                try:
-                    await asyncio.wait_for(proc.wait(), 1)
-                except asyncio.TimeoutError:
-                    proc.kill()
+            await proc.wait()
+            if proc.returncode != 0:
+                with times_path.open('a') as fd:
+                    fd.write(f'seed {seed:06d} code -15 wall ?? user ?? sys ?? mem ??kb\n')
 
 
 def main():
-    (ROOT / 'stats').mkdir(exist_ok=True)
-
+    times_path.parent.mkdir(exist_ok=True)
     loop = asyncio.new_event_loop()
     event = asyncio.Event()
-    signal.signal(signal.SIGINT, lambda *_: event.set())
+    @functools.partial(signal.signal, signal.SIGINT)
+    def stop(*_):
+        event.set()
+        print(" Stopping... ", file=sys.stderr)
     loop.run_until_complete(asyncio.wait([run(event) for _ in range(WORKERS)]))
 
 main()
